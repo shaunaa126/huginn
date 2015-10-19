ad986c09bd897f51621f04a56c7b7b5faff5fcd7
shaunaa126 <shaunaa126@gmail.com>
2015-10-18 21:05:59 -0500

    Alter Docker scripts to point to shaunaa126/huginn repo
    Added db column for user to include authentication token. Fixed docker
    scripts to point to shaunaa126/huginn repo.

--- config/application.rb
+++ config/application.rb
@@ -62,8 +62,7 @@ module Api
     config.middleware.use Rack::Cors do
       allow do
         origins "*"
-        resource "*", headers: :any, methods: [:get,
-            :post, :put, :delete, :options]
+        resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
     config.active_record.raise_in_transactional_callbacks = true
